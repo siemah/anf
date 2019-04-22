3480eb76254c86c3bb527597dc8fd684a48a764f
siemah <m_saidouni@inptic.edu.dz>
2019-04-22 20:30:39 +0200

    (Build)preinscription page UI finished

--- src/components/widgets/Jumbotron.js
+++ src/components/widgets/Jumbotron.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 import '../../assets/css/jumbotron.css';
 
-const Jumbotron = ({size='full', theme='default', className='', nextElementId='', children, ...rest}) => {
+const Jumbotron = ({size='full', theme='default', className='', nextElementId=null, children, ...rest}) => {
   return (
     <div
       className={`jumbotron jumbotron-block ${size} theme-jumbotron__${theme} ${className || ''}`}
@@ -12,11 +12,15 @@ const Jumbotron = ({size='full', theme='default', className='', nextElementId=''
 
       {children}
 
-      <a href={`#${nextElementId}`} className="jumbotron__slide-down-arrrow">
-        <svg aria-hidden="true" data-prefix="fal" className='jumbotron__slide-down-arrrow__image' data-icon="arrow-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-          <path fill={theme === 'default'? '#0ab587' : 'white'} d="M443.5 248.5l-7.1-7.1c-4.7-4.7-12.3-4.7-17 0L241 419.9V44c0-6.6-5.4-12-12-12h-10c-6.6 0-12 5.4-12 12v375.9L28.5 241.4c-4.7-4.7-12.3-4.7-17 0l-7.1 7.1c-4.7 4.7-4.7 12.3 0 17l211 211.1c4.7 4.7 12.3 4.7 17 0l211-211.1c4.8-4.8 4.8-12.3.1-17z"></path>
-        </svg>
-      </a>
+      {
+        nextElementId && (
+          <a href={`#${nextElementId}`} className="jumbotron__slide-down-arrrow">
+            <svg aria-hidden="true" data-prefix="fal" className='jumbotron__slide-down-arrrow__image' data-icon="arrow-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path fill={theme === 'default'? '#0ab587' : 'white'} d="M443.5 248.5l-7.1-7.1c-4.7-4.7-12.3-4.7-17 0L241 419.9V44c0-6.6-5.4-12-12-12h-10c-6.6 0-12 5.4-12 12v375.9L28.5 241.4c-4.7-4.7-12.3-4.7-17 0l-7.1 7.1c-4.7 4.7-4.7 12.3 0 17l211 211.1c4.7 4.7 12.3 4.7 17 0l211-211.1c4.8-4.8 4.8-12.3.1-17z"></path>
+            </svg>
+          </a>
+        )
+      }
 
     </div>
   )
--- src/pages/preinscription.js
+++ src/pages/preinscription.js
@@ -4,14 +4,87 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Jumbotron from '../components/widgets/Jumbotron'
+import { FormField, Button } from '../components/layout-components/form';
+import { Container, Row, Col } from '../components/layout-components/grid';
+
+import BGImage from '../assets/images/preregister.svg';
+import '../assets/css/preinscription.css';
 
 const PreinscriptionPage = () => (
   <Layout>
     <SEO title="Préinscription et adhérer" />
-    <Jumbotron >
-      <h1>Preinscrire</h1>
+    <Jumbotron className='preregister-hero' style={{backgroundImage: `url(${BGImage})`}}>
+      <Container className="preregister-block">
+        <Row>
+          <Col s='12' m='6' l='7' className='preregister-block__motivation-wrapper'>
+            <div className="preregister-block__motivation">
+              <h1 className="header_1">Pourquoi adhérer</h1>
+              <ul className="list-block ml0">
+                <li className="list-item">Assister à l’assemblée générale annuelle </li>
+                <li className="list-item">Accéder à la revue de l’ANF</li>
+                <li className="list-item">Bénéficier de tarifs préférentiels pour tous les évènements organisés par l’ANF</li>
+                <li className="list-item">Disposer en accès libre à un fonds documentaire avec possibilités d’acquisition d’ouvrages en liaison avec la finance ou les métiers connexes.</li>
+                <li className="list-item">Recevoir les informations par mailing sur l’ensemble des activités de l’ANF</li>
+              </ul>
+            </div>
+          </Col>
+          <Col s='12' m='6' l='5' className="preregister-block__form-wrapper">
+            <form action="#">
+              <Row>
+                <Col s='12'>
+                  <h2 className='center'>Préinscription</h2>
+                </Col>
+              </Row>
+              <Row>
+                <Col s='12' m='6'>
+                  <FormField name='nom' placeholder='Votre Nom' label='Nom*' />
+                </Col>
+                <Col s='12' m='6'>
+                  <FormField name='prenom' placeholder='Votre Prénom' label='Prénom*' />
+                </Col>
+              </Row>
+              <Row>
+                <Col s='12' m='6'>
+                  <FormField name='date-de-naissance' type='date' placeholder='JJ/MM/AAAA' label='Date De Naissance*' />
+                </Col>
+                <Col s='12' m='6'>
+                  <FormField name='lieu-de-naissance' placeholder='Cite 20 Aout 56' label='Lieu De Naissance*' />
+                </Col>
+              </Row>
+              <Row>
+                <Col s='12' m='6'>
+                  <FormField name='profession' placeholder='Manager, Professeur ..' label='Votre Profession*' />
+                </Col>
+                <Col s='12' m='6'>
+                  <FormField name='employeur' placeholder="Employeur (nom d'entreprise)" label='Employeur*' />
+                </Col>
+              </Row>
+              <Row>
+                <Col s='12'>
+                  <FormField name='adresse-professionelle' placeholder='Ben Aknoun, alger' label='Adresse Professionelle*' />
+                </Col>
+              </Row>
+              <Row>
+                <Col s='12'>
+                  <FormField name='diplome' placeholder="Doctorat En Economie étranger" label='Diplôme Et/Ou Titre*' />
+                </Col>
+              </Row>
+              <Row>
+                <Col s='12'>
+                  <label htmlFor="terms">
+                    <input id={'terms'} type='checkbox' className="form-field__checkbox" value='1' />
+                    Accêpter les <Link to='/terms'>conditions et les terms</Link> d'inscription
+                  </label>
+                </Col>
+                <Col s='12'>
+                  <Button className="button__material form__button_submit capitalize" name='envoyer'>compléter la préinscription</Button>
+                </Col>
+              </Row>
+            </form>
+          </Col>
+        </Row>
+      </Container>
     </Jumbotron>
-    <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
