6dd636411f96695b822c7382a585e4703cf4df69
siemah <m_saidouni@inptic.edu.dz>
2019-04-23 03:04:53 +0200

    (Build)404 page UI finished

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Jumbotron from '../components/widgets/Jumbotron';
+import { Container } from '../components/layout-components/grid';
+import { Button } from '../components/layout-components/form';
+
+import http404 from'../assets/images/http-404.svg'
+import '../assets/css/404.css';
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404: Pages n'existe pas :(" />
+    <Jumbotron className="page-404 center">
+      <Container >
+        <img src={http404} alt="la page n'existe pas" />
+        <p>Oops! Cette page n'existe plus.</p>
+        <Button id="btn">
+          <Link to='/' title="Retour ver la page d'accueil">
+            Mene moi vers la page d'accueil
+          </Link>
+        </Button>
+      </Container>
+    </Jumbotron>
   </Layout>
 )
 
