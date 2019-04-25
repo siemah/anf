35fffda9ba3d0825cb0b0c9366c5f8bea1e6be82
siemah <m_saidouni@inptic.edu.dz>
2019-04-25 04:55:13 +0200

    (Build)add organisation structure to presentation page ui

--- src/components/presentation/organisation.js
+++ src/components/presentation/organisation.js
@@ -8,11 +8,43 @@ import DefaultImg from '../../assets/images/a-hero-grey.png';
 
 const Organisation = props => (
   <Container className="organisation-block">
-    <Row>
-      <Col s='12' className=''>
+    <Row className='centered-dot'>
+      <Col s='12' className='center opacity-0 js-slide-from-down' data-delay={0}>
         <Card
           imageSrc={DefaultImg}
           alt="Mr. HADJAMMAR Med Akli Presedent de l'ANF"
+          title='Mr. HADJAMMAR Med Akli'
+          text="Presedent de l'ANF"
+          className='inline-block card_primary'
+        />
+      </Col>
+    </Row>
+    <Row className='centered-dot-middle'>
+      <Col s='12' m='6' className='center opacity-0 js-slide-from-down' data-delay={0.4}>
+        <Card
+          title='Mr. HADJAMMAR Med Akli'
+          text="Presedent de l'ANF"
+          className='inline-block'
+        />
+      </Col>
+      <Col s='12' m='6' className='center opacity-0 js-slide-from-down' data-delay={0.5}>
+        <Card
+          title='Mr. HADJAMMAR Med Akli'
+          text="Presedent de l'ANF"
+          className='inline-block'
+        />
+      </Col>
+    </Row>
+    <Row className='centered-dot-middle'>
+      <Col s='12' m='6' className='center opacity-0 js-slide-from-down' data-delay={0.9}>
+        <Card
+          title='Mr. HADJAMMAR Med Akli'
+          text="Presedent de l'ANF"
+          className='inline-block'
+        />
+      </Col>
+      <Col s='12' m='6' className='center opacity-0 js-slide-from-down' data-delay={1}>
+        <Card
           title='Mr. HADJAMMAR Med Akli'
           text="Presedent de l'ANF"
           className='inline-block'
@@ -20,72 +52,26 @@ const Organisation = props => (
       </Col>
     </Row>
     <Row>
-      <Col s='12' m='6' className=''>
+      <Col s='12' m='4' className='center opacity-0 js-slide-from-down' data-delay={1.4}>
         <Card
-          imageSrc={DefaultImg}
-          alt="Mr. HADJAMMAR Med Akli Presedent de l'ANF"
           title='Mr. HADJAMMAR Med Akli'
           text="Presedent de l'ANF"
           className='inline-block'
         />
       </Col>
-      <Col s='12' m='6' className=''>
+      <Col s='12' m='4' className='center opacity-0 js-slide-from-down' data-delay={1.5}>
         <Card
-          imageSrc={DefaultImg}
-          alt="Mr. HADJAMMAR Med Akli Presedent de l'ANF"
           title='Mr. HADJAMMAR Med Akli'
           text="Presedent de l'ANF"
           className='inline-block'
         />
       </Col>
-    </Row>
-    <Row>
-      <Col s='12' m='6' className=''>
-        <div className="inline-block card">
-          <div className="card__image"></div>
-          <div className="card__action-block">
-            <h2 className="card__title">Mr. HADJAMMAR Med Akli</h2>
-            <h4 className='card__text'>Presedent de l'ANF</h4>
-          </div>
-        </div>
-      </Col>
-      <Col s='12' m='6' className=''>
-        <div className="inline-block card">
-          <div className="card__image"></div>
-          <div className="card__action-block">
-            <h2 className="card__title">Mr. HADJAMMAR Med Akli</h2>
-            <h4 className='card__text'>Presedent de l'ANF</h4>
-          </div>
-        </div>
-      </Col>
-    </Row>
-    <Row>
-      <Col s='12' m='4' className=''>
-        <div className="inline-block card">
-          <div className="card__image"></div>
-          <div className="card__action-block">
-            <h2 className="card__title">Mr. HADJAMMAR Med Akli</h2>
-            <h4 className='card__text'>Presedent de l'ANF</h4>
-          </div>
-        </div>
-      </Col>
-      <Col s='12' m='4' className=''>
-        <div className="inline-block card">
-          <div className="card__image"></div>
-          <div className="card__action-block">
-            <h2 className="card__title">Mr. HADJAMMAR Med Akli</h2>
-            <h4 className='card__text'>Presedent de l'ANF</h4>
-          </div>
-        </div>
-      </Col>
-      <Col s='12' m='4' className=''>
-        <div className="inline-block card">
-          <div className="card__image"></div>
-          <div className="card__action-block">
-            <h2 className="card__title">Mr. HADJAMMAR Med Akli</h2>
-            <h4 className='card__text'>Presedent de l'ANF</h4>
-          </div>
-        </div>
+      <Col s='12' m='4' className='center opacity-0 js-slide-from-down' data-delay={1.6}>
+        <Card
+          title='Mr. HADJAMMAR Med Akli'
+          text="Presedent de l'ANF"
+          className='inline-block'
+        />
       </Col>
     </Row>
   </Container>
--- src/components/widgets/Card.js
+++ src/components/widgets/Card.js
@@ -4,15 +4,15 @@ import PropTypes from 'prop-types';
 import '../../assets/css/card.css';
 
 const Card = ({className='', imageSrc=null, alt='', title, text}) => (
-  <div className={`card ${className}`}>
+  <div className={`card ${className} ${!imageSrc? 'card_withou-image' : ''}`}>
     {
       imageSrc && (
-        <div className="card__image-block">
+        <div className="card__image-block left">
           <img src={imageSrc} className='card__image' alt={alt || title}/>
         </div>
       )
     }
-    <div className="card__action-block">
+    <div className="card__action-block left">
       <h2 className="card__title">{title}</h2>
       <h4 className='card__text'>{text}</h4>
     </div>
