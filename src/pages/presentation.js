5059b34c4946c824aef8d795bbec2544da9801ed
siemah <m_saidouni@inptic.edu.dz>
2019-04-27 01:15:56 +0200

    (Fix)fix importing file name at presentation page

--- src/pages/presentation.js
+++ src/pages/presentation.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import Organisation from '../components/presentation/Organisation'
+import Organisation from '../components/presentation/organisation'
 import Hero from '../components/presentation/Hero'
 import AboutSection from '../components/presentation/AboutSection'
 
