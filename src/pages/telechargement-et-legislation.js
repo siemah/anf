e4f632ad8589b1b6b4b9bd56acb44d0a38abfa80
siemah <m_hamiche@inptic.edu.dz>
2021-03-01 18:12:32 +0100

    feat:use latest updates from wordpress plugin

--- src/pages/telechargement-et-legislation.js
+++ src/pages/telechargement-et-legislation.js
@@ -9,30 +9,39 @@ import { Container, } from '../components/layout-components/grid';
 import AHeroBG from '../assets/images/a-hero-grey.png';
 import '../assets/css/telechargement-et-legislation.css';
 
-const DownloadsAndLegislationPage = ({ data: { allWordpressPost } }) => (
+const DownloadsAndLegislationPage = ({ data: { allWpPost } }) => (
   <Layout>
-    <SEO url={`https://anfdz.com/telechargement-et-legislation/`} title="Préinscription et adhérer" />
+    <SEO
+      url={`https://anf-dz.com/telechargement-et-legislation/`}
+      title="Préinscription et adhérer"
+      description="LEGISLATION et TEXTES REGLEMENTAIRES"
+      keywords={["LEGISLATION", "TEXTES", "REGLEMENTAIRES", "ANF"]}
+    />
     <Jumbotron className='downloads-hero skew-section-after' theme='dark' nextElementId='donwloads-items'>
       <Container className="downloads-block">
-        <h1 className="hero__title downloads-hero__title center">LEGISLATION & TEXTES REGLEMENTAIRES</h1>
+        <h1 className="hero__title downloads-hero__title center">LEGISLATION ET TEXTES REGLEMENTAIRES</h1>
       </Container>
     </Jumbotron>
     <div className='donwloads-block' style={{ backgroundImage: `url(${AHeroBG})` }} id='donwloads-items'>
       <Container>
         <ul className="list-block center">
           {
-            allWordpressPost && allWordpressPost.edges.length
-              ? allWordpressPost.edges.map(({ node }) => (
-                <li className="list-item" key={node.id}>
-                  <a
-                    className="list-item__link"
-                    target='_blank'
-                    rel="noopener noreferrer"
-                    title={node.acf.file.title}
-                    href={node.acf.file.source_url}>
-                    {node.title}
-                  </a>
-                </li>
+            allWpPost && allWpPost.edges.length
+              ? allWpPost.edges.map(({ node }) => (
+                node.acf.document
+                  ? (
+                    <li className="list-item" key={node.id}>
+                      <a
+                        className="list-item__link uppercase"
+                        target='_blank'
+                        rel="noopener noreferrer"
+                        title={node.title}
+                        href={node.acf.document.mediaItemUrl}>
+                        {node.title}
+                      </a>
+                    </li>
+                  )
+                  : null
               ))
               : <h2>Y'a pas des fichiers à téléchargies pour le moment</h2>
           }
@@ -47,24 +56,29 @@ export default DownloadsAndLegislationPage;
 export const pageQuery = graphql`
   query {
     allWpPost (
-      filter: { 
-        categories: { 
-          nodes: {
-            elemMatch: {
-              slug: {eq: "pdf"
-            }
-          }
+      filter: { categories: {
+        nodes: {
+        elemMatch: {slug: {eq: "pdf"}} }
         }
       }
-    }) {
+    sort: {
+			fields: [title]
+      order: ASC
+    }
+  ) {
       edges {
         node {
           title
           id
+          featuredImage {
+						node {
+							link
+            }
+          }
           acf {
             document {
+							sourceUrl
               mediaItemUrl
-              sourceUrl
             }
           }
         }
